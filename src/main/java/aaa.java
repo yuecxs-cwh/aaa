f32f9ec2899e8d8b8ca25148e583f405b6620aed
chenw <chenwenhao1781@163.com>
2021-04-01 18:52:44 +0800

    master

--- src/main/java/aaa.java
+++ src/main/java/aaa.java
@@ -2,5 +2,7 @@ public class aaa {
     public static void main(String[] args) {
         System.out.println("aaa");
         System.out.println("aaa1");
+        System.out.println("master");
+
     }
 }
